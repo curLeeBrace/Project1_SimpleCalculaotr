d0b285ed01f5f52cd7d4f339c46fb84a5a14c5d3
curLeeBrace <leeandreimain@gmail.com>
2023-01-18 17:36:40 +0800

    Some Error .value property

---
+++ calculator.js
@@ -0,0 +1,39 @@
+class numbers
+{
+    
+    constructor(input)
+    {
+        this.input = input;
+    }
+
+    set_input()
+    {
+       let output = document.getElementById('output');
+       output.textContent = this.input;
+    }
+
+}
+
+class one extends numbers
+{
+    constructor(input)
+    {
+        super(input);
+    }
+
+}
+class two extends numbers
+{
+    constructor(input)
+    {
+        super(input);
+    }
+}
+
+class one extends numbers
+{
+    constructor(input)
+    {
+        super(input);
+    }
+}
