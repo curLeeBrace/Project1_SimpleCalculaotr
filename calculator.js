3447f90d1faf1d73256142a91105e939a07a8965
curLeeBrace <leeandreimain@gmail.com>
2023-01-19 17:05:59 +0800

    add some input1

--- calculator.js
+++ calculator.js
@@ -1,43 +1,29 @@
-class Numbers
+
+let user_input = (input) => 
 {
-    
-    constructor(input)
+    let output = document.getElementById('output');
+
+    if(output.innerHTML == 0)
     {
-        this.input = input;
+        output.innerHTML = input;
         
-    }
-
-    set_input()
+    } else
     {
-
-      let output = document.getElementById('output');
-      output.innerHTML = this.input;
-      console.log(this.input);
-       
+        output.innerHTML += input;
     }
 
 }
 
-class One extends Numbers
-{
-    constructor(input)
-    {
-        super(input);
-    }
 
-}
-class Two extends Numbers
-{
-    constructor(input)
-    {
-        super(input);
-    }
-}
-
-
-let one = new One("1");
-let two = new Two("2");
-
-
-document.getElementById('one').addEventListener('click', () => one.set_input());
-document.getElementById('two').addEventListener('click', () => two.set_input());
+//Numbers
+let one = document.getElementById('one').addEventListener('click', () => {user_input("1")});
+let two = document.getElementById('two').addEventListener('click', () => {user_input("2")});
+let three = document.getElementById('three').addEventListener('click', () => {user_input("3")});
+let four = document.getElementById('four').addEventListener('click', () => {user_input("4")});
+let five = document.getElementById('five').addEventListener('click', () => {user_input("5")});
+let six = document.getElementById('six').addEventListener('click', () => {user_input("6")});
+let seven = document.getElementById('seven').addEventListener('click', () => {user_input("7")});
+let eight = document.getElementById('eight').addEventListener('click', () => {user_input("8")});
+let nine = document.getElementById('nine').addEventListener('click', () => {user_input("9")});
+let zero = document.getElementById('zero').addEventListener('click', () => {user_input("0")});
+let dot = document.getElementById('dot').addEventListener('click', () => {user_input(".")});
